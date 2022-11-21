4b4e360cb6cd9c88c8b1438ab34c751e5af30285
Chenhao Hu <huc17@cardiff.ac.uk>
2022-11-21 18:57:27 +0000

    feat: context init done

--- client/components/HabitList/HabitList.tsx
+++ client/components/HabitList/HabitList.tsx
@@ -1,25 +1,25 @@
 import { Pressable, View } from 'react-native';
 import { Habit } from '../../../lib/api-intefaces';
-import apiService from '../../ApiService';
+import * as apiService from '../../ApiService';
 import HabitItem from '../HabitItem/HabitItem';
 import styles from './style'
 
 export default function HabitList({ habits, selectedDate, getHabits }) {
-  const handleDelete = async (habitName : Habit) => {
-    try{
+  const handleDelete = async (habitName: Habit) => {
+    try {
       await apiService.deleteHabits(habitName, selectedDate);
       getHabits();
-    }catch(e){
+    } catch (e) {
       console.log(e)
     }
   };
   return (
     <View style={styles.container} testID="container">
       {habits &&
-        habits.map((habitName :Habit) => {
+        habits.map((habitName: Habit) => {
           return (
             <Pressable onLongPress={() => handleDelete(habitName)} key={habitName.id} testID={`habit${habitName.id}`}>
-              <HabitItem habitName={habitName} selectedDate={selectedDate} getHabits={getHabits} key={habitName.id}/>
+              <HabitItem habitName={habitName} selectedDate={selectedDate} getHabits={getHabits} key={habitName.id} />
             </Pressable>
           );
         })}
